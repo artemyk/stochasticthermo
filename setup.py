4822348cf5a6cac0be0cdf54fffdf22fb82a80c9
Artemy Kolchinsky <artemyk@gmail.com>
2022-07-22 21:55:44 +0900

    Update

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-   name='stochastic-thermo',
+   name='stochasticthermo',
    version='1.1',
    description='Stochastic thermodynamics in Python',
    author='Artemy Kolchinsky',
    author_email='artemyk@gmail.com',
-   packages=['stochastic-thermo'],
+   packages=['stochasticthermo'],
    install_requires=['numpy', 'scipy',],
 )
