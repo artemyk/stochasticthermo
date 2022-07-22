7e58bf882f01df651f744e316a4dab1c2ff41ce5
Artemy Kolchinsky <artemyk@gmail.com>
2022-07-22 23:03:16 +0900

    Update

--- stochasticthermo/__init__.py
+++ stochasticthermo/__init__.py
@@ -1,1 +1,2 @@
-from .code import *
+from .epr import *
+from .ratematrix import *
---
+++ stochasticthermo/ratematrix.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+
+def get_stationary(W): 
+    assert(np.allclose(W.sum(axis=0),0))
+
+    # get stationary distribution of rate matrix W. W[i,j] is transition rate from j->i
+    evec, evals = np.linalg.eig(W)
+    evec1ix     = np.isclose(evec,0)
+    if not (sum(evec1ix)==1):
+        raise Exception('# of eigenvalue=0 eigenvector is not 1. %s' % str(evec))
+        
+    st          = np.ravel(evals[:,evec1ix])
+    assert(np.allclose(np.imag(st), 0))
+    st          = np.real(st)
+    st         /= st.sum()
+    
+    if not np.all(st>=0):
+        raise Exception('Some stationary probabilities are negative %s' % str(st))
+        
+    assert(np.allclose(W @ st,0))
+    return st
+
+
+# def get_st(R):
+#     evals, evecs = scipy.linalg.eig(R)
+#     ixs          = np.flatnonzero(np.isclose(evals,0,atol=1e-6))
+#     if len(ixs) != 1: raise Exception()
+#     p  = evecs[:,ixs[0]]
+#     if not np.allclose(R.dot(p),0)  : raise Exception()
+#     if not np.allclose(np.imag(p),0): raise Exception()
+#     p  = np.real(p)
+#     p /= p.sum()
+#     return p
+
+
+def get_random_ratematrix(N,p, exp=1):
+    # Generate random rate matrix. Increase exp to make distribution fatter tailed
+    assert(len(p)==N)
+    W = np.random.random((N,N))**exp  # make distribution fatter tailed
+    np.fill_diagonal(W,0)
+    
+    fluxes = W*p[None,:]    
+    W     /= fluxes.sum()                    # normalize activity to 1
+    W     -= np.diag(W.sum(axis=0))
+    return W
+
+
+
