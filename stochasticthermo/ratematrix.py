3006d7ea1154b7c45a4df9665f64cfe813012d4f
Artemy Kolchinsky <artemy@Artemys-MacBook-Pro.local>
2023-02-09 22:20:08 +0900

    eps fix

--- stochasticthermo/ratematrix.py
+++ stochasticthermo/ratematrix.py
@@ -1,12 +1,14 @@
 import numpy as np
 import scipy.linalg
 
+eps = 1e-5
+
 def get_stationary(W): 
-    assert(np.allclose(W.sum(axis=0),0))
+    assert(np.all(np.abs(W.sum(axis=0))<=eps))
 
     # get stationary distribution of rate matrix W. W[i,j] is transition rate from j->i
     evec, evals = np.linalg.eig(W)
-    evec1ix     = np.isclose(evec,0)
+    evec1ix     = np.abs(evec)<=eps
     if not (sum(evec1ix)==1):
         raise Exception('# of eigenvalue=0 eigenvector is not 1. %s' % str(evec))
         
@@ -15,16 +17,16 @@ def get_stationary(W):
     st          = np.real(st)
     st         /= st.sum()
     
-    st[np.logical_and(st<0, st>-1e-10)] = 0
+    st[np.logical_and(st<0, st>-eps)] = 0
     if not np.all(st>=0):
         raise Exception('Some stationary probabilities are negative %s' % str(st))
         
-    assert(np.allclose(W @ st,0))
+    assert(np.all(np.abs(W @ st)<=eps))
     return st
 
 # def get_st(R):
 #     evals, evecs = scipy.linalg.eig(R)
-#     ixs          = np.flatnonzero(np.isclose(evals,0,atol=1e-6))
+#     ixs          = np.flatnonzero(np.isclose(evals,0,atol=eps))
 #     if len(ixs) != 1: raise Exception()
 #     p  = evecs[:,ixs[0]]
 #     if not np.allclose(R.dot(p),0)  : raise Exception()
