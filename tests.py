b55cfef44d6c3cc79d2c49fef61721460f282969
Artemy Kolchinsky <artemyk@gmail.com>
2024-07-23 23:09:17 -0500

    Added numerical_radius

--- tests.py
+++ tests.py
@@ -25,3 +25,13 @@ W3= st.get_random_ratematrix(N=3, p_st=p_st2)
 
 
 st.get_unicyclic_ratematrix([1,2,3],[2,3,5])
+
+
+
+# Test numerical radius
+
+N = 10
+A = np.random.random((N,N))
+A = A+A.T
+max_ev = np.abs(np.linalg.eigvals(A)).max()
+assert(np.isclose(max_ev, st.numerical_radius(A)))
